4d692c9efc93c9937c918afa69de77fbec819bb0
MadeByDroids <andy@Mhi-1400.local>
2016-03-30 22:13:38 +0100

    Changed a bunch of stuff
    removed masonry dynamic load, replaced with css hard coded images
    removed bootstrap and modal window

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,6 @@
 
 
-jQuery(document).ready(function($) {  
+jQuery(document).ready(function($) {
 	// site preloader -- also uncomment the div in the header and the css style for #preloader
 	$(window).load(function(){
 		$('#preloader').fadeOut('slow',function(){$(this).remove();});
@@ -34,65 +34,9 @@ $(window).on("resize load", resizeWindow);
 		 });
 }
 
-// masonry
-$( function() {
 
-  var $container = $('#container').masonry({
-    itemSelector: '.item',
-    columnWidth: 250,
-    isFitWidth: true
-  });
-
-  $('#load-images').click( function showPics() {
-    var $items = getItems();
-    // hide by default
-    $items.hide();
-    // append to container
-    $container.append( $items );
-    $items.imagesLoaded().progress( function( imgLoad, image ) {
-      // get item
-      // image is imagesLoaded class, not <img>
-      // <img> is image.img
-      var $item = $( image.img ).parents('.item');
-      // un-hide item
-      $item.show();
-      // masonry does its thing
-      $container.masonry( 'appended', $item );
-    });
-  });
-  
-});
-
-function randomInt( min, max ) {
-  return Math.floor( Math.random() * max + min );
-}
-
-function getItem() {
- var width = randomInt( 300, 800 );  
- var height = randomInt( 300, 500 );
-   // var width = 100
-  //var height = 100
-  var item = '<div class="item"><button class="js-button btn btn-secondary-outline center-block" data-toggle="modal" data-target="#modalPicture" type="button" value="Expand photo" role="button">Expand photo</button>'+'<img src="http://lorempixel.com/' +  width + '/' + height + '/nature" /></div>';
-  return item;
-}
-
-function getItems() {
-  var items = '';
-  for ( var i=0; i < 12; i++ ) {
-    items += getItem();
-  }
-  // return jQuery object
-  return $( items );
-}
-
-// click image functionality
-$(document).on("click", ".js-button", function() {
-    var imageSrc = $(this).parents(".item").find("img").attr("src");
-    $(".js-download").attr("href", imageSrc);
-    $(".js-modal-image").attr("src", imageSrc);
-    $(document).on("click", ".js-heart", function() {
-      $(this).toggleClass("active");
-    });
-  });
-
-
+	new AnimOnScroll( document.getElementById( 'grid' ), {
+		minDuration : 0.4,
+		maxDuration : 0.7,
+		viewportFactor : 0.2
+	} );
